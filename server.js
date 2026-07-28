b7acf1a200f1611440962b5f0841907920092bbc
Enoma joel <enomajoel8@gmail.com>
2026-07-28 22:26:31 +0100

    Add host routes to the server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const filegenRoutes = require('./b8ai-filegen-routes');
+const hostRoutes = require('./b8ai-host-route');
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(filegenRoutes);
+app.use(hostRoutes);
 
 app.get('/', (req, res) => res.send('B8AI Filegen Service is running'));
 app.get('/healthz', (req, res) => res.send('ok'));
